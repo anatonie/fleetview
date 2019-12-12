e42bba7d14bf1b3a97e5f6e6b6edc219fb86a812
anatonie <anatonie@amazon.com>
2019-12-12 11:39:22 -0800

    Adding controls to fleetview

--- src/routes/fleetview.js
+++ src/routes/fleetview.js
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import Form from 'react-bootstrap/Form'
 
 import * as OrgFleet from '../utilities/orgFleet';
 import { getModels } from '../utilities/fleetview';
 
+const defaultScale = 2.5;
+
 export default function Fleetview() {
     const [models, setModels] = useState();
     const [fleet, setFleet] = useState();
+    const [scale, setScale] = useState(defaultScale);
+    const [rotate, setRotate] = useState(false);
 
-    let scale = 2;
     const wrapperPadding = 10;
     const maxWidth = window.innerWidth - (4 * wrapperPadding);
 
@@ -20,8 +24,37 @@ export default function Fleetview() {
         return <div>Loading</div>
     }
     return (
-        <div>
-            <div style={{padding: wrapperPadding + 'px', maxWidth: '90%'}}>
+        <div style={{padding: wrapperPadding + 'px', maxWidth: '90%', margin: '0 auto'}}>
+            <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+                <div>
+                    <div style={{display: 'flex'}}>
+                        <label style={{marginRight: '0.5rem'}}>Scale</label>
+                        <input
+                            type="range"
+                            className="custom-range"
+                            value={scale}
+                            onChange={(event) => setScale(event.target.value)}
+                            min={0.1}
+                            max={10}
+                            step={0.1}
+                            style={{maxWidth: '10rem'}}
+                        />
+                    </div>
+                    <div style={{display: 'flex'}}>
+                        <label style={{marginRight: '0.5rem'}}>Rotate</label>
+                        <Form.Check value={rotate} onChange={() => setRotate(!rotate)}/>
+                    </div>
+                </div>
+            </div>
+            <div
+                style={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    alignItems: 'center',
+                    alignContent: 'center',
+                    justifyContent: 'space-around'
+                }}
+            >
                 {fleet
                     .map((ship) => ({
                         ship: ship,
@@ -32,7 +65,7 @@ export default function Fleetview() {
                     .map(({ship, model}, idx) => {
                         let width = scale * model.beam;
                         let height = scale * model.length;
-                        if (idx === 0 && height > maxWidth) {
+                        if (rotate && idx === 0 && height > maxWidth && scale === defaultScale) {
                             scale = maxWidth / model.length;
                             width = scale * model.beam;
                             height = scale * model.length;
@@ -44,8 +77,8 @@ export default function Fleetview() {
                                 key={idx}
                                 style={{
                                     float: 'left',
-                                    width: height,
-                                    height: width,
+                                    width: rotate ? height : undefined,
+                                    height: rotate ? width : undefined,
                                     verticalAlign: 'middle',
                                     margin: wrapperPadding + 'px'
                                 }}
@@ -56,8 +89,10 @@ export default function Fleetview() {
                                 style={{
                                     height,
                                     width,
-                                    transform: 'rotate(90deg) translateX(-' + (scale * model.length) + 'px)',
-                                    transformOrigin: 'bottom left'
+                                    transform: rotate ?
+                                        'rotate(90deg) translateX(-' + (scale * model.length) + 'px)' :
+                                        'rotate(180deg)',
+                                    transformOrigin: rotate ? 'bottom left' : undefined
                                 }}
                             />
                         </span>
